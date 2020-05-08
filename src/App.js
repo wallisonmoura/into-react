0a30f63856a11fc10fab449f5819c41c67c749a8
wallisonmoura <wallison_cedaspy@hotmail.com>
2020-05-07 21:16:15 -0300

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import "./App.css";
+
+import TechList from "./components/techList";
 
 function App() {
-  return <h1>Hello World, Dev!</h1>;
+  return <TechList />;
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: { loader: "file-loader" },
+      },
     ],
   },
 };
